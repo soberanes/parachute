5ff6ef692980e25610b565cd7354e6588210ea34
Paul Soberanes <paulsoberanes@gmail.com>
2013-07-19 02:06:27 -0500

    Update jquery.parachute.js
    Text indent

--- js/jquery.parachute.js
+++ js/jquery.parachute.js
@@ -1,54 +1,54 @@
 (function($){
   $.fn.parachute = function(options){
 
-					$(this).each(function() {
-						defaults = {
-				    		share_url: $(this).attr('href'),
-				    		network: $(this).attr('data-network'),
-				    		tweet_via: $(this).attr('data-via'),
-				    		tweet_description: $(this).next('.description').text(),
-				    		title: 'Popup Dialog',
-				    		width: '400',
-				    		height: '200'
-				    	}
+			$(this).each(function() {
+				defaults = {
+		    		share_url: $(this).attr('href'),
+		    		network: $(this).attr('data-network'),
+		    		tweet_via: $(this).attr('data-via'),
+		    		tweet_description: $(this).next('.description').text(),
+		    		title: 'Popup Dialog',
+		    		width: '400',
+		    		height: '200'
+		    	}
 
-				    	var options = $.extend({}, defaults, options), complete_link, no_network;
+		    	var options = $.extend({}, defaults, options), complete_link, no_network;
 
-						$(this).click(function(){
-					    	
-					    	
+				$(this).click(function(){
+			    	
+			    	
 
-					    	if( options.network == 'facebook' ){
-					    		
-					    		complete_link = 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(options.share_url);
-					    	
-					    	}else if ( options.network == 'twitter' ) {
-					    		
-					    		complete_link = 'https://twitter.com/intent/tweet?';
-					    		
-					    		if( options.tweet_via != '' ){
-					    			complete_link += 'via=' + options.tweet_via + '&';
-					    		}
-					    		
-					    		complete_link += 'url=' + options.share_url;
+			    	if( options.network == 'facebook' ){
+			    		
+			    		complete_link = 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(options.share_url);
+			    	
+			    	}else if ( options.network == 'twitter' ) {
+			    		
+			    		complete_link = 'https://twitter.com/intent/tweet?';
+			    		
+			    		if( options.tweet_via != '' ){
+			    			complete_link += 'via=' + options.tweet_via + '&';
+			    		}
+			    		
+			    		complete_link += 'url=' + options.share_url;
 
-					    		if( options.tweet_description != '' ){
-					    			complete_link += '&text=' + options.tweet_description + ' ';
-					    		}
+			    		if( options.tweet_description != '' ){
+			    			complete_link += '&text=' + options.tweet_description + ' ';
+			    		}
 
 
-					    	}else{
-					    		no_network = true;
-					    		console.log('no network selected');
-					    	}
-						   	if(!no_network){
-								window.open(
-									complete_link,
-									options.title,
-									'width='+options.width+',height='+options.height+''
-								);
-							}
-							return false;
+			    	}else{
+			    		no_network = true;
+			    		console.log('no network selected');
+			    	}
+				   	if(!no_network){
+						window.open(
+							complete_link,
+							options.title,
+							'width='+options.width+',height='+options.height+''
+						);
+					}
+					return false;
 	        			});
 	        }); 
 	}
